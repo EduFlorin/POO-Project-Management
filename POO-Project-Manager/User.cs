3cff36a334a24764985fc2d99155dca510162be7
EduFlorin <florin-gheorghe.edu@student.upt.ro>
2026-01-08 20:11:24 +0200

    Create User.cs

---
+++ POO-Project-Manager/User.cs
@@ -0,0 +1,17 @@
+public enum UserRole
+{
+    Manager,
+    Member
+}
+public class User
+{
+    public string Username {get; set;}
+    public string Password {get; set;}
+    public UserRole Role {get; set;}
+    public User(string username, string password, UserRole role)
+    {
+        Username=username;
+        Password=password;
+        Role=role;
+    }
+}
